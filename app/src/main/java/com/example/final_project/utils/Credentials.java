c94be6374600e0cba0b547e8497e94ed5207c0f5
muchtar <muchtaradam2002@gmail.com>
2023-06-10 18:17:49 +0800

    feat : add MovieApi and MovieResponse

--- app/src/main/java/com/example/final_project/utils/Credentials.java
+++ app/src/main/java/com/example/final_project/utils/Credentials.java
@@ -1,6 +1,6 @@
 package com.example.final_project.utils;
 
-public class Credentioals {
+public class Credentials {
     public static final String BASE_URL = "https://api.tmdb.org/3/";
     public static final String KEY = "45cf6f17c3b027741256fb8c7ecfeaa7";
     public static final String POSTER_URL = "https://image.tmdb.org/t/p/w500/";
---
+++ app/src/main/java/com/example/final_project/utils/MovieApi.java
@@ -0,0 +1,13 @@
+package com.example.final_project.utils;
+
+import com.example.final_project.response.MovieResponses;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface MovieApi {
+    @GET("movie/popular")
+    Call<MovieResponses> getPopularMovie(@Query("api_key") String key,
+                                         @Query("page") int page);
+}
