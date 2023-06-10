c77488381851e3db1d09f25be1f8c49578a3c980
muchtar <muchtaradam2002@gmail.com>
2023-06-10 20:15:51 +0800

    feat : update bottom nav bar main_activity.xml

--- app/src/main/java/com/example/final_project/MainActivity.java
+++ app/src/main/java/com/example/final_project/MainActivity.java
@@ -1,16 +1,50 @@
 package com.example.final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.example.final_project.fragment.FavoriteFragment;
+import com.example.final_project.fragment.MovieFragment;
+import com.example.final_project.fragment.TvShowFragment;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout btn_movie, btn_tvShow, btn_favorite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btn_movie = findViewById(R.id.btn_movies);
+        btn_tvShow = findViewById(R.id.btn_tvShows);
+        btn_favorite = findViewById(R.id.btn_favorites);
 
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        MovieFragment movieFragment = new MovieFragment();
+        Fragment fragment = fragmentManager.findFragmentByTag(MovieFragment.class.getSimpleName());
+        if (!(fragment instanceof MovieFragment)) {
+            fragmentManager.beginTransaction().add(R.id.fl_main, movieFragment,
+                    MovieFragment.class.getSimpleName()).commit();
+        }
+
+        btn_movie.setOnClickListener(v -> switchFragment(new MovieFragment()));
+        btn_tvShow.setOnClickListener(v -> switchFragment(new TvShowFragment()));
+        btn_favorite.setOnClickListener(v -> switchFragment(new FavoriteFragment()));
+    }
+
+    private void switchFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        if (fragment instanceof MovieFragment) {
+            transaction.replace(R.id.fl_main, fragment, MovieFragment.class.getSimpleName()).commit();
+        } else {
+            transaction.replace(R.id.fl_main, fragment, MovieFragment.class.getSimpleName()).addToBackStack(null).commit();
+        }
     }
 }
--- app/src/main/java/com/example/final_project/fragment/TvShowFragment.java
+++ app/src/main/java/com/example/final_project/fragment/TvShowFragment.java
@@ -1,4 +1,4 @@
-package com.example.final_project;
+package com.example.final_project.fragment;
 
 import android.os.Bundle;
 
@@ -8,12 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MoviesFragment extends Fragment {
+import com.example.final_project.R;
 
+public class TvShowFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movies, container, false);
+        return inflater.inflate(R.layout.fragment_tv_show, container, false);
     }
 }
