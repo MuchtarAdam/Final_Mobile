f3e5981bf105521cbb9766a30014df2dbe7419b5
muchtar <muchtaradam2002@gmail.com>
2023-06-10 18:04:18 +0800

    feat : add AppExecutor

--- app/src/main/java/com/example/final_project/model/MoviesModel.java
+++ app/src/main/java/com/example/final_project/model/MoviesModel.java
@@ -1,6 +1,11 @@
 package com.example.final_project.model;
 
-public class MoviesModel {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+public class MoviesModel implements Parcelable {
     private int id;
     private String title;
     private String backdrop_image;
@@ -18,6 +23,28 @@ public class MoviesModel {
         this.vote_average = vote_average;
         this.overView = overView;
     }
+
+    protected MoviesModel(Parcel in) {
+        id = in.readInt();
+        title = in.readString();
+        backdrop_image = in.readString();
+        poster_image = in.readString();
+        release_date = in.readString();
+        vote_average = in.readFloat();
+        overView = in.readString();
+    }
+
+    public static final Creator<MoviesModel> CREATOR = new Creator<MoviesModel>() {
+        @Override
+        public MoviesModel createFromParcel(Parcel in) {
+            return new MoviesModel(in);
+        }
+
+        @Override
+        public MoviesModel[] newArray(int size) {
+            return new MoviesModel[size];
+        }
+    };
 
     public int getId() {
         return id;
@@ -45,5 +72,34 @@ public class MoviesModel {
 
     public String getOverView() {
         return overView;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel parcel, int i) {
+        parcel.writeInt(id);
+        parcel.writeString(title);
+        parcel.writeString(backdrop_image);
+        parcel.writeString(poster_image);
+        parcel.writeString(release_date);
+        parcel.writeFloat(vote_average);
+        parcel.writeString(overView);
+    }
+
+    @Override
+    public String toString() {
+        return "MoviesModel{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", backdrop_image='" + backdrop_image + '\'' +
+                ", poster_image='" + poster_image + '\'' +
+                ", release_date='" + release_date + '\'' +
+                ", vote_average=" + vote_average +
+                ", overView='" + overView + '\'' +
+                '}';
     }
 }
--- app/src/main/java/com/example/final_project/response/MovieResponses.java
+++ app/src/main/java/com/example/final_project/response/MovieResponses.java
@@ -1,5 +1,24 @@
 package com.example.final_project.response;
 
-public class MovieResponses {
+import com.example.final_project.model.MoviesModel;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
+
+public class MovieResponses {
+    @SerializedName("results")
+    @Expose
+    private List<MoviesModel> movies;
+
+    public List<MoviesModel> getMovies() {
+        return movies;
+    }
+
+    @Override
+    public String toString() {
+        return "MovieResponses{" +
+                "movies=" + movies +
+                '}';
+    }
 }
