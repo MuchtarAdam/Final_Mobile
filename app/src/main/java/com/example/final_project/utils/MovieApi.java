b85ecb9c70e460c3f61ab864ca2724481ad91cc3
muchtar <muchtaradam2002@gmail.com>
2023-06-11 18:50:38 +0800

    feat : add build apk

--- app/src/main/java/com/example/final_project/utils/MovieApi.java
+++ app/src/main/java/com/example/final_project/utils/MovieApi.java
@@ -9,13 +9,16 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface MovieApi {
-
-//    https://api.themoviedb.org/3/search/movie?api_key=45cf6f17c3b027741256fb8c7ecfeaa7&query=Jack+Reacher
-    @GET("3/movie/popular/{movie_id}?")
-    Call<MovieModel> getMovie(
-            @Query("api_key") String key,
-            @Query("page") int page
+    @GET Call<MovieSearchResponse> searchMovie(
+      @Query("key") String key,
+      @Query("query") String query,
+      @Query("page") int pageNumber
     );
 
-    Call<MovieSearchResponse> searchMovie(String key, String query, int pageNumber);
+    @GET("movie/popular/")
+    Call<MovieModel> getMovie(
+            @Query("api_key") String key,
+            @Query("query") String query,
+            @Query("page") int page
+    );
 }
