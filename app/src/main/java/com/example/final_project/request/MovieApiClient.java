70c0101e3b5dc2a1394e5be22ea0c93e61f2d7de
muchtar <muchtaradam2002@gmail.com>
2023-06-11 12:35:06 +0800

    feat : add AppExecutors and update MovieApiClient

--- app/src/main/java/com/example/final_project/request/MovieApiClient.java
+++ app/src/main/java/com/example/final_project/request/MovieApiClient.java
@@ -3,9 +3,12 @@ package com.example.final_project.request;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.example.final_project.AppExecutors;
 import com.example.final_project.models.MovieModel;
 
 import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class MovieApiClient {
     private MutableLiveData<List<MovieModel>> mMovies;
@@ -24,5 +27,22 @@ public class MovieApiClient {
     }
     public LiveData<List<MovieModel>> getMovies(){
         return mMovies;
+    }
+
+
+    public void searchMoviesApi(){
+        final Future myHandler = AppExecutors.getInstance().networkIO().submit(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+
+        AppExecutors.getInstance().networkIO().schedule(new Runnable() {
+            @Override
+            public void run() {
+                myHandler.cancel(true);
+            }
+        },4000, TimeUnit.MICROSECONDS);
     }
 }
